74f47f3ac85e46b88335486786c24e3918195eb1
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2025-02-14 17:19:24 +0800

    修改js

--- delete_from_test.js
+++ delete_from_test.js
@@ -1,4 +1,3 @@
-const xlsx = require('xlsx');
 const mysql = require('mysql');
 
 // 创建数据库连接
@@ -17,26 +16,15 @@ connection.connect(err => {
   }
   console.log('成功连接到数据库，ID:', connection.threadId);
 
-  // 读取Excel文件
-  const workbook = xlsx.readFile('蓝盾读写导入模板.xlsx');
-  const sheetName = workbook.SheetNames[0]; // 获取第一个工作表的名字
-  const worksheet = workbook.Sheets[sheetName];
-  const jsonData = xlsx.utils.sheet_to_json(worksheet); // 将工作表转换为JSON格式
-
-  // 准备插入语句
-  const insertDataQuery = `INSERT INTO test (name) VALUES ?`;
-  
-  // 提取数据准备插入
-  const dataToInsert = jsonData.map(row => [row.name]);
-  
-  // 执行插入操作
-  connection.query(insertDataQuery, [dataToInsert], (error, results, fields) => {
-    if (error) {
-      console.error('数据插入失败:', error.stack);
-      return;
+  // 执行删除操作
+  connection.query('DELETE FROM test', (deleteErr, deleteResult) => {
+    if (deleteErr) {
+      console.error('删除操作失败:', deleteErr.stack);
+    } else {
+      console.log('删除成功:', deleteResult.affectedRows, '行受到影响');
     }
 
-    console.log('数据插入成功');
+    // 关闭数据库连接
     connection.end(() => {
       console.log('数据库连接已关闭');
     });
